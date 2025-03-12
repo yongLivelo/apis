3af01a91a5a8403d8ae4e272151836dda56fa7fb
Wizo101 <winson.yap2006@gmail.com>
2025-03-12 15:29:51 +0800

    edithtbl

--- front/src/pages/Biodata/Biodata.tsx
+++ front/src/pages/Biodata/Biodata.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@gmail.com",
+    },
+  ];
+}
+
 export default function Biodata() {
+  const [data, setData] = useState<Payment[]>([]);
+
+  useEffect(() => {
+    getData().then((fetchedData) => {
+      setData(fetchedData);
+    });
+  }, []);
+
   return (
-    <>
-      <div className="flex justify-center h-screen bg-accent">
-        <div>We are coding at Biodata</div>
-        {/* Add Code Below */}
-        Winson aynandito Julio is here
-      </div>
-    </>
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
   );
 }
