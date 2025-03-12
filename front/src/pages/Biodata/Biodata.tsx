aadb2d32bd364efd0e63c4fd9a81bc971759dd08
Julio Eduardo B. Livelo <94818606+yongLivelo@users.noreply.github.com>
2025-03-12 16:04:35 +0800

    fix

--- front/src/pages/Biodata/Biodata.tsx
+++ front/src/pages/Biodata/Biodata.tsx
@@ -25,18 +25,16 @@ export default function Biodata() {
   }, []);
 
   return (
-
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
-
     <>
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+
       <div className="flex justify-center h-screen bg-accent">
         <div>We are coding at Biodata</div>
         {/* Add Code Below */}
         Winson aynandito Julio is here Joachim is here WHAT THE FUCK
       </div>
     </>
-
   );
 }
